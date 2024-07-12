66f189246c63296dac5541124025e9a53d09573d
KoliRamonee <161612645+KoliRamonee@users.noreply.github.com>
2024-07-11 21:55:22 -0300

    Update login.js

--- javaScript/login.js
+++ javaScript/login.js
@@ -8,7 +8,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Verificar el email y la contraseña
     if (email === "admin@admin.com" && password === "Admin123") {
       alert("¡Inicio de sesión correcto! Bienvenido: " + email);
-      alert("Puedes entrar a la base de datos usando la URL+/usuariosDB.html")
+      alert("Puedes entrar a la base de datos usando la URL+//usuariosDB.html")
       // Aquí podrías redireccionar a otra página o realizar alguna otra acción después del inicio de sesión exitoso
     } else {
       alert("Correo electrónico o contraseña incorrectos. Por favor, inténtelo de nuevo.");
